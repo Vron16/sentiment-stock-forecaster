50f382a6f2a0225ba4bc35c4d203f9884951434d
Amany Elgarf <35881779+AmanyElgarf@users.noreply.github.com>
2019-04-20 17:18:32 -0400

    Update test copy.py

--- WEBSITE/test copy.py
+++ WEBSITE/test copy.py
@@ -1,34 +1,32 @@
 from flask import Flask, get_template_attribute, render_template, url_for, request, redirect, jsonify
 from SentimentController import SentimentController
+from flask import *
+from SentimentController import SentimentController
+import json
 
 app = Flask(__name__)
+
+
+@app.route('/testFunc/', methods=['GET', 'POST'])
+def testFunc():
+    SentController = SentimentController()
+    data = request.args.get('stockName', 0, type=str)
+    headlines = SentController.requestHeadlines(data)
+    k = testFunc1()
+    array = [headlines, k]
+    return jsonify(array)
+
+
+def testFunc1():
+    sentController = SentimentController()
+    data = request.args.get('stockName', 0, type=str)
+    returned = sentController.finalToUser(data)
+    return returned
 
 
 @app.route('/')
 def lionel():
     return render_template('index.html')
-
-
-@app.route('/testFunc', methods=['GET', 'POST'])
-def testFunc():
-    # SentController = SentimentController()
-    data = request.args.get('stockName', 0, type=str)
-    print(data)
-    #returned = SentController.finalToUser(data)
-    #headlines = SentController.requestHeadlines(data)
-    #printedHeadlines = SentController.printHeadlines(headlines)
-    #print(printedHeadlines)
-    #return render_template("jsonn.html", result=data)
-    #Data = get_template_attribute('index.html', 'Data')
-    hello = get_template_attribute('index.html', 'hello')
-    k = (hello('World'))
-    print (k)
-    return k
-
-
-
-
-
 
 
 if __name__ == '__main__':
