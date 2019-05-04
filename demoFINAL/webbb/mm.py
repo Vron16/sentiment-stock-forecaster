086aad32372976b385b4364b7b7e14b87c637534
ParkerFisher <33067153+ParkerFisher@users.noreply.github.com>
2019-05-04 16:52:46 -0400

    Add files via upload

---
+++ demoFINAL/webbb/mm.py
@@ -0,0 +1,326 @@
+### PLEASE MAKE SURE YOU HAVE THE FOLlOWING PACKAGES:
+### - STATSMODELS
+### - SCIPY
+### DATELINE SHOULD BE A DEFAULT PACKAGE
+from statsmodels.tsa.arima_model import ARIMA
+import datetime
+import numpy as np
+
+from numpy import fft
+
+# ----------------- Rate of Change ------------------#
+
+numDays = 5
+
+
+def calculateROC(closing, now, nAgo):
+    # calculate the ROC between two times (Formulas in report)
+    diff = closing[now] - closing[nAgo]
+    roc = (diff / closing[nAgo]) * 100
+    return roc
+
+
+def nDayRateOfChange(now, closing):
+    # get all of the ROCs for all numDays periods into a list, return it
+    rocs = []
+    i = 0
+    while i < len(closing) - numDays:
+        answer = calculateROC(closing, i, i + numDays)
+        rocs.append(answer)
+        i += numDays
+    return rocs
+
+
+def printRocs(rocs):
+    # helper function for unit tests
+    print("ROCS:")
+    for r in rocs:
+        print(r)
+
+
+def getClosingPrices(times, list):
+    # get the closing prices for all days
+    # because the stock market closes at 4pm, check if times[i].hour == 16
+    prices = []
+    for i in range(len(times)):
+        if times[i].hour == 16:
+            prices.append(list[i])
+    return prices
+
+
+def getRateOfChange(stockData):
+    # store the time stamps and the closing prices
+    times = stockData[0]
+    fiveMinList = stockData[4]
+
+    # get the rocs for a given period
+    closing = getClosingPrices(times, fiveMinList)
+    rocs = nDayRateOfChange(0, closing)
+    return rocs
+
+
+# ----------------- Stochastic Oscillator ------------------#
+
+def calculateSO(closing, high, low, cur, now, nAgo):
+    # calculate the stochastic oscillator for a given period (Formulas in report)
+    highPrice = findHighest(high, now, nAgo)
+    lowPrice = findLowest(low, now, nAgo)
+
+    numer = cur - lowPrice
+    denom = highPrice - lowPrice
+    return (numer / denom) * 100
+
+
+def findHighest(high, now, nAgo):
+    # find the highest price in a list
+    highPrice = 0
+    i = nAgo
+    while i >= now:
+        if high[i] > highPrice:
+            highPrice = high[i]
+
+        i -= 1
+    return highPrice
+
+
+def findLowest(low, now, nAgo):
+    # find the lowest price in a list
+    lowPrice = float("inf")
+    i = nAgo
+    while i >= now:
+        if low[i] < lowPrice:
+            lowPrice = low[i]
+
+        i -= 1
+    return lowPrice
+
+
+def nDayOscillation(num, closing, high, low, cur):
+    # for every numDay period, get the stochastic oscillator, store in a list
+    sos = []
+    i = 0
+    while i < len(closing) - num:
+        answer = calculateSO(closing, high, low, cur, i, i + num)
+        sos.append(answer)
+        i += num
+    return sos
+
+
+def printSO(so):
+    # helper function for unit tests
+    print("SO:")
+    for s in so:
+        print(s)
+
+
+def getClosingPrices(times, list):
+    # get the closing price for all days
+    prices = []
+    for i in range(len(times)):
+        if times[i].hour == 16:
+            prices.append(list[i])
+    return prices
+
+
+def getHighPrices(times, list):
+    # get the highest price for all days
+    prices = []
+    highest = 0
+    for i in range(len(times)):
+        if list[i] > highest:
+            highest = list[i]
+        if times[i].hour == 16:
+            prices.append(highest)
+            highest = 0
+    return prices
+
+
+def getLowPrices(times, list):
+    # get the lowest price for all days
+    prices = []
+    lowest = float("inf")
+    for i in range(len(times)):
+        if list[i] < lowest:
+            lowest = list[i]
+        if times[i].hour == 16:
+            prices.append(lowest)
+            lowest = float("inf")
+    return prices
+
+
+def getCurPrice(list):
+    # newest price at the beginning of list
+    return list[0]
+
+
+def getStochasticOscillator(stockData):
+    # store the time stamps, high prices, low prices, and closing prices for every five minute interval
+    times = stockData[0]
+    highFiveMinList = stockData[1]
+    lowFiveMinList = stockData[2]
+    fiveMinList = stockData[4]
+
+    # prices list and current price
+    closing = getClosingPrices(times, fiveMinList)
+    high = getHighPrices(times, highFiveMinList)
+    low = getLowPrices(times, lowFiveMinList)
+    cur = getCurPrice(fiveMinList)
+
+    # get list of stock oscillations for all numDay peridss
+    so = nDayOscillation(numDays, closing, high, low, cur)
+    return so
+
+
+# -------------------ASI--------------------
+
+def getSwing(stock_Data, priceSamplesPerDay, priceSamplesToday):
+    length = len(stock_Data)
+    t = 100  # t has to be user defined, not sure what a good number is
+    O = stock_Data[length - 1]  # today's closing price
+    Oy = stock_Data[length - 1 - priceSamplesToday]  # yesterdays closing price
+    C = stock_Data[length - priceSamplesToday]  # today's opening price
+    Cy = stock_Data[length - priceSamplesToday - priceSamplesPerDay]  # yesterdays opening price
+
+    yesterdaysPrices = []  # make a list of yesterdays prices
+    i = 0  # counter
+    Hy = 0  # yesterdays high price
+    Ly = Cy  # yesterdays low price
+    while i < priceSamplesPerDay:
+        yesterdaysPrices.append(stock_Data[length - priceSamplesToday - priceSamplesPerDay + i])
+        if yesterdaysPrices[i] > Hy:
+            Hy = yesterdaysPrices[i]
+        if yesterdaysPrices[i] < Ly:
+            Ly = yesterdaysPrices[i]
+        i += 1
+
+    # Perform the ASI calculations (Formulas in report)
+    todaysPrices = []
+    i = 0
+    H = 0
+    L = C
+    while i < priceSamplesToday:
+        todaysPrices.append(stock_Data[length - priceSamplesToday + i])
+        if todaysPrices[i] > H:
+            H = todaysPrices[i]
+        if todaysPrices[i] < L:
+            L = todaysPrices[i]
+        i += 1
+
+    K = max(H - Cy, Cy - L)
+    TR = max(H - Cy, L - Cy, H - L)
+
+    if TR == H - Cy:
+        R = (H - C) - .5 * (L - C) - .25 * (Cy - Oy)
+    elif TR == L - Cy:
+        R = L - Cy - .5 * (H - O) + .25 * (Cy - Oy)
+    else:
+        R = H - L + .25 * (Cy - Oy)
+
+    swing = 50 * (Cy - C + .5 * (Cy - Oy) + .25 * (C - O) / R) * K / t
+
+    return swing
+
+
+# To get ASI you get the swing for every day in the dataset
+
+def getASI(stock_Data):
+    # store the time stamps and the high prices
+    times = stock_Data[0]
+    high_prices = stock_Data[1]
+
+    todaySamples = 0
+    yesterdaySamples = 0
+
+    ASI = []
+
+    # Find all of the prices for a single day, call getSwing() to get Swing Index for that day
+    # Append the swing for each day into ASI
+    for k in range(len(times) - 1):
+        todaySamples += 1
+        if times[k + 1].hour == 16:
+            if yesterdaySamples != 0 and todaySamples != 0:
+                ASI.append(getSwing(high_prices[0: k], yesterdaySamples, todaySamples))
+            yesterdaySamples = todaySamples
+            todaySamples = 0
+
+    ASI.reverse()
+
+    return ASI
+
+
+# ----------------- ARIMA ------------------#
+
+
+def getARIMA(stock_data):
+    # fc will hold the forecast made by ARIMA
+    fc = -1
+    if (len(stock_data) < 1):  # if the list is empty, return -1
+        return fc
+
+    # p is a parameter for the ARIMA model, and can act as a looping iterator
+    p = 5
+    while (1):
+        try:  # we try to fit the data to the ARIMA model, with p initially at 5
+            if (p == 0):  # base case condition: if we looped five times already,
+                # there wasn't enough stock data to make a meaningful prediction, return -2
+                return -2
+            model = ARIMA(stock_data, order=(p, 1, 0))
+            model_fit = model.fit(disp=0)
+            # if the model fits, we can ask it to make a prediction, and return it
+            fc = model_fit.forecast()[0][0]
+            return fc
+        except:  # the model could not fit, so we try again by decrementing p
+            p -= 1
+
+    return fc
+
+
+def fourierExtrapolation(stock_data, n_predict):
+    n = stock_data.size
+    n_harm = 10  # number of harmonics in model
+    t = np.arange(0, n)
+    p = np.polyfit(t, stock_data, 1)  # find linear trend in x
+    stock_data_notrend = stock_data - p[0] * t  # detrended x
+    stock_data_freqdom = fft.fft(stock_data_notrend)  # detrended x in frequency domain
+    f = fft.fftfreq(n)  # frequencies
+    indexes = list(range(n))
+    # sort indexes by frequency, lower -> higher
+    indexes.sort(key=lambda i: np.absolute(f[i]))
+
+    t = np.arange(0, n + n_predict)
+    restored_sig = np.zeros(t.size)
+    for i in indexes[:1 + n_harm * 2]:
+        ampli = np.absolute(stock_data_freqdom[i]) / n  # amplitude
+        phase = np.angle(stock_data_freqdom[i])  # phase
+        restored_sig += ampli * np.cos(2 * np.pi * f[i] * t + phase)
+    return restored_sig + p[0] * t
+
+
+def getFourier(stock_data):
+    np_stock_data = np.array(stock_data)
+    n_predict = 3
+    extrapolation = fourierExtrapolation(np_stock_data, n_predict)
+
+    print(extrapolation)
+    print(extrapolation[-3])
+    print(extrapolation[-2])
+    print(extrapolation[-1])
+
+    predicted = []
+    for i in range(n_predict):
+        predicted.append(extrapolation[-1 * (n_predict - i)])
+    return predicted
+
+
+def aggregatePrediction(roc, stoch_os, asi, curPrice, arima_prediction, fourier_prediction):
+    # This is our averager. For the purpose of the demo here we printed all of our results and returned the ARIMA prediction
+
+    print("Rate of change: ", roc[0])
+    print("Stochastic Oscillator: ", stoch_os[0])
+    print("Accumulative Swing Index: ", asi[0])
+    print("Current Price: ", curPrice)
+    print("Unweighted ARIMA Prediction: ", arima_prediction)
+    print("Unweighted Fourier Predictions: ", fourier_prediction)
+
+    return [arima_prediction, fourier_prediction]
+
---
+++ demoFINAL/webbb/test copy.py
@@ -0,0 +1,183 @@
+from flask import Flask, get_template_attribute, render_template, url_for, request, redirect, jsonify
+from SentimentController import SentimentController
+from flask import *
+from Database import Database
+import techcontroller
+import mysql.connector as ms
+import json
+from ATController import AutomatedTrader as AT
+
+useremail = ""
+
+app = Flask(__name__)
+
+
+
+class DataStore():
+    useremail = None
+    portfolio = {}
+
+data = DataStore()
+
+@app.route('/addUser/',methods=['GET','POST'])
+def addUser():
+    db = Database()
+    email = request.args.get('useremail', 0, type=str)
+    password = request.args.get('userpassword', 0, type=str)
+    data.useremail = email
+    outputstr = db.addUser(email,password)
+    return jsonify(outputstr)
+
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    db = Database()
+    email = request.args.get('loginemail', 0, type=str)
+    data.useremail = email
+    if email == "parker@gmail.com":
+        data.portfolio = {'AAPL':3,'IBM':2,'NFLX':10}
+    elif email == "ui6@gmail.com":
+        data.portfolio = {'A':20,'AMZN':1,'ABC':10,'ACM':3}
+    else:
+        data.portfolio = {}
+
+
+    password = request.args.get('loginpassword', 0, type=str)
+    loginstr = db.validateLogin(email,password)
+    print("main email is", useremail)
+    loginstr.append(len(data.portfolio))
+    for k, v in data.portfolio.items():
+        print("You currently own " + str(v) + " " + str(k) + " share(s).")
+        loginstr.append(str(k))
+        loginstr.append(str(v))
+    print(loginstr)
+    return jsonify(loginstr)
+
+
+
+@app.route('/deposit/',methods=('GET','POST'))
+def deposit():
+    db = Database()
+    email = data.useremail
+
+    amt = request.args.get('depositamt', 0, type=str)
+    result = db.deposit(email,amt)
+    result.append(email)
+    return jsonify(result)
+
+@app.route('/withdraw/',methods=('GET','POST'))
+def withdraw():
+    db = Database()
+
+    email = data.useremail
+    amt = request.args.get('withdrawamt', 0, type=str)
+
+    result = db.withdraw(email,amt)
+    result.append(email)
+
+    return jsonify(result)
+
+@app.route('/testFunc/', methods=['GET', 'POST'])
+def testFunc():
+    print('Hello')
+    SentController = SentimentController()
+    userInput = request.args.get('stockName', 0, type=str) #user input which is the stock ticker
+    print(userInput)
+    webScrapeInput = SentController.getStockTickers(userInput) #returns the database stock name if it's there
+    print(webScrapeInput)
+    if webScrapeInput == '':
+        return jsonify('Error: Stock ticker not in our database. Please choose a NASDAQ stock.')
+    print(webScrapeInput)
+    #headlines = SentController.requestHeadlines(webScrapeInput)
+    #print(headlines)
+    #if len(headlines) < 5:
+    patterns = [" Common Stock", ",", " -", " (", " Inc ", " Inc.", " Corp.", " Corp ", " Common Stock ", " Ltd.", " Ltd", " Limited", " LLC", " ESG", "ETF", " REIT", " plc"]
+    for pat in patterns: #strip the unnecessary terms from the database stock name
+        webScrapeInput = webScrapeInput.split(pat, 1)[0]
+    print(webScrapeInput)
+    stockNameHeadlines = SentController.requestHeadlines(webScrapeInput) #retrieve all headlines with parsed stock name
+    stockTickerHeadlines = SentController.requestHeadlines(userInput) #retrieve all headlines with stock ticker
+    totalHeadlines = stockNameHeadlines + stockTickerHeadlines #now we have all the headlines from both stock ticker search and db stock name search!
+    print(totalHeadlines)
+    #only keep the headlines with either stock ticker or webScrapeInput
+    parsedHeadlines = []
+    for headline in totalHeadlines:
+        if webScrapeInput in headline or userInput in headline:
+            parsedHeadlines.append(headline)
+    print(parsedHeadlines)
+    if len(parsedHeadlines) < 5:
+        return jsonify([])
+    #headlineDeviations = {} #dict that will contain headlines with deviation from average
+    #avgScore = SentController.calcAvgSentScore(parsedHeadlines) #average score of all the headlines
+    #for headline in parsedHeadlines:
+     #   headlineDeviations[headline] = abs(avgScore - SentController.calculateSingleScore(headline))
+    #print(headlineDeviations)
+    #sortedHeadlineDeviations = sorted(headlineDeviations.items(), key=itemgetter(1))
+    #print(sortedHeadlineDeviations)
+    #sortedHeadlines = []
+    #for headlineDeviation in sortedHeadlineDeviations:
+     #   sortedHeadlines.append(headlineDeviation[0])
+    #print(sortedHeadlines)
+    SentController.trend(userInput)
+    k = testFunc1(parsedHeadlines)
+    array = [parsedHeadlines, k]
+    return jsonify(array)
+
+def testFunc1(scraperInput):
+    sentController = SentimentController()
+    returned = sentController.finalToUser(scraperInput)
+    return returned
+
+@app.route('/techFunc/', methods=['GET', 'POST'])
+def techFunc():
+    data = request.args.get('stockNameTech', 0, type=str)
+    print(data)
+    arr = techcontroller.getPrediction(data)
+    if not arr:
+        return json.dumps([])
+    return json.dumps(arr)
+@app.route('/autotrade/',methods=['GET','POST'])
+def run_autotrader():
+    # '''autotrader method'''
+    print("print runauto")
+    ATStockName = request.args.get('ATStockName', 0, type=str)
+    durationDays = request.args.get('durationDays', 0, type=int)
+    durationHrs = request.args.get('durationHrs', 0, type=int)
+    durationDMin = request.args.get('durationMin', 0, type=int)
+
+    seconds = 60*durationDMin + 3600*durationHrs + 86400*durationDays
+    db = Database()
+
+    email = data.useremail
+    userbal = db.getbal(email)
+
+
+    autotrader = AT()
+    at_out = autotrader.auto_trade(ATStockName, seconds,userbal,data.portfolio)
+    #autotrader=AT.auto_trade(ATStockName,seconds)
+
+    print(at_out)
+
+    return jsonify(at_out)
+
+
+@app.route('/loginn/', methods=['GET', 'POST'])
+def loginn():
+    return render_template('login.html')
+
+@app.route('/loginb/', methods=['GET', 'POST'])
+def loginb():
+    return render_template('logout.html')
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    return render_template('index.html')
+
+
+
+@app.route('/')
+def lionel():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
